93ce7939caeeebdcaeffa05f04ca111ecf88e6fe
jralduaveuthey <jaime.raldua.veuthey@gmail.com>
2022-12-01 11:40:10 +0100

    update

--- deployment_PrefectFargate/infrastructure/delete_all_prefect_blocks.py
+++ deployment_PrefectFargate/infrastructure/delete_all_prefect_blocks.py
@@ -1,32 +1,15 @@
 import subprocess
+import sys
+   
 
-if __name__ == "__main__":
-    blocks = [
-        "dbt-cli-profile/default",
-        "dbt/attribution",
-        "dbt/jaffle-shop",
-        "dbt-cloud-credentials/default",
-        "snowflake-connector/default",
-        "snowflake-credentials/default",
-        "snowflake-pandas/default",
-        "workspace/default",
-        "azure/default",
-        "s3/default",
-        "gcs/default",
-        "docker-container/default",
-        "kubernetes-job/default",
-        "process/default",
-        "slack-webhook/default",
-        "github/default",
-        "github/dbt-jaffle-shop",
-        "github/dbt-attribution",
-    ]
+def delete_blocks(blocks):   
     for block in blocks:
-        subprocess.run(f"prefect block delete {block}", shell=True)
-    block_types = [
-        "workspace",
-        "snowflake-schema",
-        "dbt",
-    ]
-    for block_type in block_types:
-        subprocess.run(f"prefect block type delete {block_type}", shell=True)
+        subprocess.run(f"prefect block delete {block}", shell=True)    
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        blocks =  list(sys.argv[1:])
+        delete_blocks(blocks)
+    else:
+        raise SystemExit("No block list passed.")
